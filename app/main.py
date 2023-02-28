5be56835a8fc2e5671e00a6793cfe18b082b4a91
Samuel Chun <sachun@hubspot.com>
2023-02-27 21:36:17 -0800

    test

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 # Uncomment this to pass the first stage
-# import socket
+import socket
 
 
 def main():
@@ -7,9 +7,8 @@ def main():
     print("Logs from your program will appear here!")
 
     # Uncomment this to pass the first stage
-    #
-    # server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    # server_socket.accept() # wait for client
+    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
+    server_socket.accept() # wait for client
 
 
 if __name__ == "__main__":
