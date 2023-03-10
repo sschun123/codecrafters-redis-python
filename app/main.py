a570e4cac7609fb2d36fec3054fcc9417cf1d3df
Samuel Chun <sachun@hubspot.com>
2023-03-09 21:27:46 -0800

    Supports multiple clients simultaneously

--- app/main.py
+++ app/main.py
@@ -1,12 +1,19 @@
 # Uncomment this to pass the first stage
 import socket
-import time
+import threading
 
 class RESP:
     @staticmethod
     def simple_string(str):
         return '+' + str + '\r\n'
 
+def create_client(clientsocket):
+    while True:
+            data = clientsocket.recv(1024)
+            if not data:
+                break
+            clientsocket.send(RESP.simple_string('PONG').encode())
+    clientsocket.close()
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -15,14 +22,12 @@ def main():
     # Uncomment this to pass the first stage
     # Convenience function which creates a SOCK_STREAM type socket bound to *address* (a 2-tuple (host, port)) and return the socket object.
     with socket.create_server(("localhost", 6379), reuse_port=True) as server_socket:
-        # skip the initial connection msg
-        (clientsocket, addr) = server_socket.accept() # wait for client
+        print('waiting for new connection...')
         while True:
-            data = clientsocket.recv(1024)
-            if not data:
-                break
-            clientsocket.send(RESP.simple_string('PONG').encode())
-        clientsocket.close()
+            (clientsocket, addr) = server_socket.accept() # wait for client
+            print('incoming connection from ', addr)
+            client_thread = threading.Thread(target=create_client, args=(clientsocket,))
+            client_thread.start()
 
 if __name__ == "__main__":
     main()
